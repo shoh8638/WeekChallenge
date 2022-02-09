a8949b86c63a233fe35b21b6e4bbe700cf919fe1
shoh8638 <shoh8638@gmail.com>
2022-02-09 16:03:38 +0900

    FEAT : 로그인 및 회원가입 시, 인디케이터 추가

--- WeekChallenge/Service/AuthModel.swift
+++ WeekChallenge/Service/AuthModel.swift
@@ -7,27 +7,35 @@
 
 import Foundation
 import FirebaseAuth
+import SwiftOverlays
 
 class AuthModel {
     func login(email: String, pwd: String, vc: UIViewController) {
+        vc.showTextOverlay("please Wait....")
         Auth.auth().signIn(withEmail: email, password: pwd) { authResult, error in
             if authResult != nil {
                 let alert = UIAlertController(title: "알림", message: "로그인 성공", preferredStyle: .alert)
                 let action = UIAlertAction(title: "확인", style: .default, handler: nil)
                 alert.addAction(action)
-                vc.present(alert, animated: true, completion: nil)
+                vc.present(alert, animated: true) {
+                    vc.removeAllOverlays()
+                }
             } else {
                 let alert = UIAlertController(title: "알림", message: "로그인 실패", preferredStyle: .alert)
                 let action = UIAlertAction(title: "확인", style: .default, handler: nil)
                 alert.addAction(action)
-                vc.present(alert, animated: true, completion: nil)
+                vc.present(alert, animated: true) {
+                    vc.removeAllOverlays()
+                }
             }
         }
     }
     
     func signIn(email: String, pwd: String, vc: UIViewController) {
+        vc.showTextOverlay("please Wait....")
         Auth.auth().createUser(withEmail: email, password: pwd) { authResult, error in
             if authResult != nil {
+                print("SignInView Success")
                 let loginVC = vc.storyboard?.instantiateViewController(withIdentifier: "LoginView")
                 loginVC?.modalPresentationStyle = .fullScreen
                 loginVC?.modalTransitionStyle = .crossDissolve
@@ -35,13 +43,20 @@ class AuthModel {
                 let alert = UIAlertController(title: "알림" , message: "회원가입 완료!" , preferredStyle: .alert)
                 let action = UIAlertAction(title: "확인", style: .default) { _ in
                     vc.present(loginVC!, animated: true, completion: nil)
+                    vc.removeAllOverlays()
                 }
                 alert.addAction(action)
+                vc.present(alert, animated: true) {
+                    vc.removeAllOverlays()
+                }
             } else {
-                print("signIn Fail")
-                let alert = UIAlertController(title: "알림" , message: "회원가입 완료!" , preferredStyle: .alert)
+                print("SignInView Failure")
+                let alert = UIAlertController(title: "알림" , message: "회원가입 실패!" , preferredStyle: .alert)
                 let action = UIAlertAction(title: "확인", style: .default, handler: nil)
                 alert.addAction(action)
+                vc.present(alert, animated: true) {
+                    vc.removeAllOverlays()
+                }
             }
         }
     }
