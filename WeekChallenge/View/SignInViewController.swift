aec6c2d896bafb5a613221c68671cc9fad3a9d81
shoh8638 <shoh8638@gmail.com>
2022-02-09 17:54:20 +0900

    FEAT :  버튼 비/활성화 관련 추가

--- WeekChallenge/View/SignInViewController.swift
+++ WeekChallenge/View/SignInViewController.swift
@@ -12,17 +12,35 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var pwdText: UITextField!
+    @IBOutlet weak var signInBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.saveBtn(isOn: false)
     }
+    
 }
 
 //MARK: Button
 extension SignInViewController {
-    @IBAction func OKBtn(_ sender: Any) {
-        let signIn = AuthModel()
-        signIn.signIn(email: emailText.text!, pwd: pwdText.text!, vc: self)
+    @IBAction func changeBtnColor(_ sender: UITextField) {
+        if sender.text?.isEmpty == true {
+            self.saveBtn(isOn: false)
+        } else {
+            self.saveBtn(isOn: true)
+            let signIn = AuthModel()
+            signIn.signIn(email: emailText.text!, pwd: pwdText.text!, vc: self)
+        }
+        
+    }
+    func saveBtn(isOn: Bool) {
+        switch isOn {
+        case true :
+            signInBtn.isEnabled = true
+            signInBtn.backgroundColor = UIColor(red: 255.0, green: 22.0, blue: 84.0, alpha: 0)
+        case false :
+            signInBtn.isEnabled = false
+            signInBtn.backgroundColor = .lightGray
+        }
     }
 }
-
