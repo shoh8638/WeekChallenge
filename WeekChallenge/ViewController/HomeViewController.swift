9689fa4bbc4d17eb49f220e6d32e4c39c8e13ce5
shoh <shoh8638@gmail.com>
2022-02-11 17:49:21 +0900

    FEAT: HomeView 컬렉션 뷰 추가

--- WeekChallenge/ViewController/HomeViewController.swift
+++ WeekChallenge/ViewController/HomeViewController.swift
@@ -13,6 +13,8 @@ class HomeViewController: UIViewController {
     let arr: Array<String> = []
     
     @IBOutlet weak var homeView: UIView!
+    @IBOutlet weak var hiddenView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     lazy var emptyView : EmptyView = {
         let view = EmptyView()
@@ -37,4 +39,22 @@ class HomeViewController: UIViewController {
     @objc func Click(sender: UIButton? = nil) {
         print("Tap")
     }
+    @IBAction func expandBtn(_ sender: Any) {
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
+        cell.backgroundColor = .black
+        return cell
+    }    
+}
+
+class Cell: UICollectionViewCell {
+    
 }
