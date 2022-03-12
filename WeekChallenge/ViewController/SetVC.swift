f855d7abf3cf86ba8a5c189454a30aebae7d20da
shoh <shoh8638@gmail.com>
2022-03-12 11:14:44 +0900

    TEST: SettingVC UI

--- WeekChallenge/ViewController/SetVC.swift
+++ WeekChallenge/ViewController/SetVC.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class SetVC: UIViewController {
-
+    
+    @IBOutlet weak var imgBackView: UIView!
     @IBOutlet weak var dddd: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imgBackView.layer.cornerRadius = 20
+        imgBackView.layer.masksToBounds = true
+        
         dddd.layer.cornerRadius = 20
         dddd.layer.masksToBounds = true
     }
